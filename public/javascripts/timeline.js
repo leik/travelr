e813299118f26c25a9018e5b546e7b500f42de50
Lei Kang <lei.kang@orionhealth.com>
2013-08-08 13:02:39 +1200

    Added keyboard and mouse wheel events.

--- public/javascripts/timeline.js
+++ public/javascripts/timeline.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
-	var cards = $("div.card-space div.card");
-	var currentCard = cards.last().addClass('current');
+	var cards = $("div.card-space div.card"),
+		currentIndex = cards.last().data('index'),
+		timeCheckers = $('ul.time li.checker'),
+		revisionCheckers = $('ul.revisions li.checker');
 
 	function initialize() {
-		renderLastTenCards(currentCard.data('index'));
+		renderLastTenCards(currentIndex);
 
-		$('li.checker').hover(function() {
+		$('li.checker').click(function() {
 			var index = $(this).data('index');
 			renderLastTenCards(index);
 		});
@@ -14,28 +16,59 @@ $(document).ready(function() {
 			var index = $(this).data('index');
 			renderLastTenCards(index);
 		});
+
+		$(document).keydown(function(event) {
+			console.log(currentIndex);
+			switch (event.keyCode) {
+				case 38:
+					renderLastTenCards(currentIndex - 1);
+					break;
+				case 40:
+					renderLastTenCards(currentIndex + 1);
+					break;
+			}
+		});
+
+		$('ul.time, ul.revisions').mousewheel(function(event,delta, deltaX, deltaY) {
+			if (delta > 0 ){
+				renderLastTenCards(currentIndex - 1);
+			} else if (delta < 0){
+				renderLastTenCards(currentIndex + 1);
+			}
+		});
+
 	}
 
-
 	function getLastTenCards(current) {
-		var elements = current.prevAll().slice(0, 9).get();
+		var elements = current.prevAll().slice(0, 9).get().reverse();
 		elements.push(current);
 		return $(elements);
 	}
 
 
 	function renderLastTenCards(selectedCardIndex) {
-		currentCard.removeClass('current');
-		currentCard = cards.eq(selectedCardIndex).addClass('current');
-		currentCard.nextAll().hide();
+		if (selectedCardIndex && selectedCardIndex >= 0 && selectedCardIndex < cards.size()) {
+			var oldSelectedIndex = currentIndex;
 
-		currentCard.prevAll().hide();
-		getLastTenCards(currentCard).each(function(index) {
-			$(this).show().css({
-				'-webkit-transform': 'translateZ(' + 60 * index + 'px)',
-				'opacity': 0.1 * (index + 1)
+			currentIndex = selectedCardIndex;
+
+			cards.eq(oldSelectedIndex).removeClass('current');
+			timeCheckers.eq(oldSelectedIndex).removeClass('current');
+			revisionCheckers.eq(oldSelectedIndex).removeClass('current');
+
+			var currentCard = cards.eq(selectedCardIndex).addClass('current');
+			timeCheckers.eq(selectedCardIndex).addClass('current');
+			revisionCheckers.eq(selectedCardIndex).addClass('current');
+
+			currentCard.nextAll().hide();
+			currentCard.prevAll().hide();
+			getLastTenCards(currentCard).each(function(index) {
+				$(this).show().css({
+					'-webkit-transform': 'translateZ(' + 60 * index + 'px)',
+					'opacity': 0.1 * (index + 1)
+				});
 			});
-		});
+		}
 	}
 
 	initialize();
